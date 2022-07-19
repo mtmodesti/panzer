7a58220f200a9512fd257c010311a4bcfd2ee0f9
mtmodesti <marcelomodesti1988@gmail.com>
2022-07-19 19:18:30 -0300

    feat: is no longer possible update cpf

--- users/serializers.py
+++ users/serializers.py
@@ -18,5 +18,16 @@ class UserSerializer(serializers.ModelSerializer):
         }
 
 
+
+    def update(self, instance: User, validated_data):
+        fixedFields = ['cpf']
+        for key, value in validated_data.items():
+            if key not in fixedFields:
+                setattr(instance, key, value)
+                instance.save()
+
+        return instance
+        ...
+
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
